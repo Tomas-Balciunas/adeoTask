947c2f1b08c4ea9e2bf5d42ccee135dc5a6e037a
Tomas-Balciunas <tomas.balciunas@stud.kitm.lt>
2021-10-01 11:28:25 +0300

    clean up

--- resources/views/blades/home.blade.php
+++ resources/views/blades/home.blade.php
@@ -1,13 +1,6 @@
 @extends('main')
 @section('content')
 <div id="cont">
-@if(!empty($errors->first()))
-    <div class="row col-lg-12">
-        <div class="alert alert-danger">
-            <span>{{ $errors->first() }}</span>
-        </div>
-    </div>
-@endif
 <a href="/api/products">List of products</a>
 <form method="POST" action="/api/products/recommendation">
 {{csrf_field()}}
